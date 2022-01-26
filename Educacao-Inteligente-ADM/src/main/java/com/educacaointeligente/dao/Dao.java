b841b1552bf6bd1c93f477599a3a3c4539486270
Davi_Nascimento <davi-ds15@hotmail.com>
2022-01-25 21:22:43 -0300

    ConfiguracaoDao
    Configuracao do Dao

---
+++ Educacao-Inteligente-ADM/src/main/java/com/educacaointeligente/dao/Dao.java
@@ -0,0 +1,18 @@
+package com.educacaointeligente.dao;
+
+import java.util.List;
+
+public interface Dao<T> {
+	
+      T get(int id);
+      
+      List<T>getAll();
+      
+      List<T>getAllWhere(int id);
+      
+      void save(T objeto);
+      
+      void update(T objeto);
+      
+      void delete (T objeto);
+}
---
+++ Educacao-Inteligente-ADM/src/main/java/com/educacaointeligente/dao/JPAUtil.java
@@ -0,0 +1,22 @@
+package com.educacaointeligente.dao;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	 private static final String PERSISTENCE_UNIT_NAME = "puEducacaoInteligente";
+	    private static EntityManagerFactory factory;
+
+	    public static EntityManagerFactory getEntityManagerFactory() {
+	        if (factory == null) {
+	            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+	        }
+	        return factory;
+	    }
+
+	    public static void shutdown() {
+	        if (factory != null) {
+	            factory.close();
+	        }
+	    }
+}
