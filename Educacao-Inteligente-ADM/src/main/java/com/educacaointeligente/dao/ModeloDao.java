5302fafdb989d7595f7ab25036911fe50c8857db
Davi_Nascimento <davi-ds15@hotmail.com>
2022-01-25 21:38:35 -0300

    Create ModeloDao.java
    Modelo_Dao

---
+++ Educacao-Inteligente-ADM/src/main/java/com/educacaointeligente/dao/ModeloDao.java
@@ -0,0 +1,59 @@
+package com.educacaointeligente.dao;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public class ModeloDao implements Dao<Object> {
+	private EntityManager em;
+    
+    public ModeloDao(){
+    	em = JPAUtil.getEntityManagerFactory().createEntityManager();
+    }
+   
+    private void ExecuteInsideTrasaction(Consumer<EntityManager>action) {
+        EntityTransaction tx = em.getTransaction();
+        try {
+        	tx.begin();
+        	action.accept(em);
+        	tx.commit();
+        }catch(RuntimeException e) {
+        	tx.rollback();
+        	throw e;
+        }
+    }
+    
+	@Override
+	public Object get(int id) {	
+		return em.find(Object.class,id);
+	}
+
+	@Override
+	public List<Object> getAll() {
+		return em.createQuery("From 'Classe'",Object.class).getResultList();
+	}
+	
+	@Override
+	public List<Object> getAllWhere(int id) {
+		
+		return em.createQuery("From Objeto Where usuario_id="+id,Object.class).getResultList();
+	}
+	
+	@Override
+	public void save (Object objeto) {
+		ExecuteInsideTrasaction(em->em.persist(objeto));
+	}
+
+	@Override
+	public void update(Object objeto) {
+		ExecuteInsideTrasaction(em->em.merge(objeto));
+	}
+
+	@Override
+	public void delete(Object objeto) {
+		ExecuteInsideTrasaction(em->em.remove(objeto));
+	}
+}
+
